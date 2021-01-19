0fa8078a073b2c4d780a7cc2438856ea2d672d07
Alex <aleib.start@gmail.com>
2021-01-19 11:53:20 +0200

    fix seo

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="Prototype App using Next.js" />
+          <meta name="keywords" content="PWA, Typescript, Next, Next.js, React" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
