86b7c6e10c1250a0d6ae0d161a81857c449cae3c
Alex <aleib.start@gmail.com>
2021-01-19 11:37:30 +0200

    updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
     runtimeCaching,
+    disable: process.env.NODE_ENV === "development",
+    register: true,
   },
-})
+});
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Link from 'next/link'
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Hello Next.js 👋</h1>
+        <p>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  );
+}
